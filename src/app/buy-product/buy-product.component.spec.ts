c67730a95c1276f746a4a6f25677290ec4da9f42
PhamDinhQuan <19130183@st.hcmuaf.edu.vn>
2023-08-07 16:57:17 +0700

    Update cart buy

--- src/app/buy-product/buy-product.component.spec.ts
+++ src/app/buy-product/buy-product.component.spec.ts
@@ -1,25 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { BuyProductComponent } from './buy-product.component';
+ import { ComponentFixture, TestBed } from '@angular/core/testing';
+ 
+ import { BuyProductComponent } from './buy-product.component';
+ 
+ describe('BuyProductComponent', () => {
+   let component: BuyProductComponent;
+   let fixture: ComponentFixture<BuyProductComponent>;
+ 
+   beforeEach(async () => {
+     await TestBed.configureTestingModule({
+       declarations: [ BuyProductComponent ]
+     })
+     .compileComponents();
+   });
+ 
+   beforeEach(() => {
+     fixture = TestBed.createComponent(BuyProductComponent);
+     component = fixture.componentInstance;
+     fixture.detectChanges();
+   });
+ 
+   it('should create', () => {
+     expect(component).toBeTruthy();
+   });
+ });
 
-describe('BuyProductComponent', () => {
-  let component: BuyProductComponent;
-  let fixture: ComponentFixture<BuyProductComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ BuyProductComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BuyProductComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+ 
